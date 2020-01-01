02fd0fb3fc40a79fde8e2e6424d8dbd7c6956b2f
vitgrey <fedyay.vitaliy@gmail.com>
2020-01-01 11:05:39 +0200

    get nav & footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { ArticlesComponent } from './components/articles/articles.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    ArticlesComponent
+    ArticlesComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
